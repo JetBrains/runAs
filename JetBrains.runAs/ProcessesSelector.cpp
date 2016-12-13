062d6440acb61dd3253da046f1614583e06031dc
Nikolay.Pianikov <Nikolay.Pianikov@jetbrains.com>
2016-12-13 12:01:58 +0300

    add docs and comments

--- JetBrains.runAs/ProcessesSelector.cpp
+++ JetBrains.runAs/ProcessesSelector.cpp
@@ -36,43 +36,42 @@ const Result<queue<const IProcess*>> ProcessesSelector::SelectProcesses(const Se
 	trace < L"ProcessesSelector::SelectProcesses statistic.GetIntegrityLevel() = ";
 	trace << statistic.GetIntegrityLevel();
 			
-	if (statistic.IsService())
+	if (statistic.HasAdministrativePrivileges())
 	{
-		auto elevationIsRequired = settings.GetIntegrityLevel() == INTEGRITY_LEVEL_SYSTEM || settings.GetIntegrityLevel() == INTEGRITY_LEVEL_HIGH;
-		trace < L"ProcessesSelector::SelectProcesses elevationIsRequired = ";
-		trace << elevationIsRequired;
-
-		// For Local System services (INTEGRITY_LEVEL_SYSTEM) and services under admin (INTEGRITY_LEVEL_HIGH)
-		// Has SeAssignPrimaryTokenPrivilege (Replace a process-level token)
-		// Has SeTcbPrivilegePrivilege (Act as part of the operating system)
-		// Has Administrative Privileges
-		if (statistic.HasAdministrativePrivileges() && statistic.HasSeAssignPrimaryTokenPrivilege() && statistic.HasSeTcbPrivilegePrivilege())
+		// Has administrative privileges
+		if (statistic.IsService())
 		{
-			trace < L"ProcessesSelector::SelectProcesses push ProcessAsUser";
-			processes.push(&_processAsUser);
+			// Is a service: for Local System services (INTEGRITY_LEVEL_SYSTEM) and services under admin (INTEGRITY_LEVEL_HIGH)
+			auto elevationIsRequired = settings.GetIntegrityLevel() == INTEGRITY_LEVEL_SYSTEM || settings.GetIntegrityLevel() == INTEGRITY_LEVEL_HIGH;
+			trace < L"ProcessesSelector::SelectProcesses elevationIsRequired = ";
+			trace << elevationIsRequired;
+			
+			if (statistic.HasSeAssignPrimaryTokenPrivilege() && statistic.HasSeTcbPrivilegePrivilege())
+			{
+				// Has SeAssignPrimaryTokenPrivilege (Replace a process-level token)
+				// Has SeTcbPrivilegePrivilege (Act as part of the operating system)
+				trace < L"ProcessesSelector::SelectProcesses push ProcessAsUser";
+				processes.push(&_processAsUser);
+			}
+		}
+		else
+		{
+			// Is not a service
+			auto changeIntegrityLevel = settings.GetIntegrityLevel() != INTEGRITY_LEVEL_DEFAULT;
+			trace < L"ProcessesSelector::SelectProcesses changeIntegrityLevel = ";
+			trace << changeIntegrityLevel;
+
+			if (changeIntegrityLevel)
+			{
+				// Changing windows integrity level
+				trace < L"ProcessesSelector::SelectProcesses push ProcessWithLogon to change Integrity Level";
+				processes.push(&_processWithLogonToChangeIntegrityLevel);
+			}
 		}
 	}
-	else
-	{
-		auto changeIntegrityLevel = settings.GetIntegrityLevel() != INTEGRITY_LEVEL_DEFAULT;
-		trace < L"ProcessesSelector::SelectProcesses changeIntegrityLevel = ";
-		trace << changeIntegrityLevel;
 
-		// Not a Service
-		// For interactive admin accounts
-		// Will be noninteractive
-		if (changeIntegrityLevel && statistic.HasAdministrativePrivileges())
-		{
-			trace < L"ProcessesSelector::SelectProcesses push ProcessWithLogon to change Integrity Level";
-			processes.push(&_processWithLogonToChangeIntegrityLevel);
-		}
-	}
-
-	// For interactive Windows accounts
-	// Integrity Level by default
-	// Will be interactive
-	// Can't be elevated
 	// All other cases
+	// Windows Integrity Level will be by default and can't be elevated
 	trace < L"ProcessesSelector::SelectProcesses push ProcessWithLogon";
 	processes.push(&_processWithLogon);
 	return processes;
